6d51a96a7ef42d68d6d7940cd9096a555a7f4045
benoit74 <benoit74@users.noreply.github.com>
2023-08-16 09:53:54 +0200

    Do not make a distinction between ruff and black args + do not use pytest-cov + do not export HTML report by default

--- tasks.py
+++ tasks.py
@@ -16,58 +16,53 @@ def test(ctx: Context, args: str = ""):
 @task(optional=["args"], help={"args": "pytest additional arguments"})
 def test_cov(ctx: Context, args: str = ""):
     """run test vith coverage"""
-    ctx.run(f"pytest --cov=src {args}", pty=use_pty)
+    ctx.run(f"coverage run -m pytest {args}", pty=use_pty)
 
 
-@task(optional=["no-html"], help={"no-html": "flag to not export html report"})
-def report_cov(ctx: Context, *, no_html: bool = False):
+@task(optional=["html"], help={"html": "flag to export html report"})
+def report_cov(ctx: Context, *, html: bool = False):
     """report coverage"""
     ctx.run("coverage combine", warn=True, pty=use_pty)
     ctx.run("coverage report --show-missing", pty=use_pty)
-    if not no_html:
+    if html:
         ctx.run("coverage html", pty=use_pty)
 
 
 @task(
-    optional=["args", "no-html"],
+    optional=["args", "html"],
     help={
         "args": "pytest additional arguments",
-        "no-html": "flag to not export html report",
+        "html": "flag to export html report",
     },
 )
-def coverage(ctx: Context, args: str = "", *, no_html: bool = False):
+def coverage(ctx: Context, args: str = "", *, html: bool = False):
     """run tests and report coverage"""
-    test_cov(ctx, args=args + " --cov-report xml")
-    report_cov(ctx, no_html=no_html)
+    test_cov(ctx, args=args)
+    report_cov(ctx, html=html)
 
 
-@task(
-    optional=["args"], help={"args": "linting tools (black, ruff) additional arguments"}
-)
+@task(optional=["args"], help={"args": "black additional arguments"})
 def lint_black(ctx: Context, args: str = "."):
     ctx.run("black --version", pty=use_pty)
     ctx.run(f"black --check --diff {args}", pty=use_pty)
 
 
-@task(
-    optional=["args"], help={"args": "linting tools (black, ruff) additional arguments"}
-)
+@task(optional=["args"], help={"args": "ruff additional arguments"})
 def lint_ruff(ctx: Context, args: str = "."):
     ctx.run("ruff --version", pty=use_pty)
     ctx.run(f"ruff check {args}", pty=use_pty)
 
 
 @task(
-    optional=["black_args", "ruff_args"],
+    optional=["args"],
     help={
-        "black_args": "linting (fix mode) black arguments",
-        "ruff_args": "linting (fix mode) ruff arguments",
+        "args": "linting tools (black, ruff) additional arguments, typically a path",
     },
 )
-def lintall(ctx: Context, black_args: str = ".", ruff_args: str = "."):
+def lintall(ctx: Context, args: str = "."):
     """Check linting"""
-    lint_black(ctx, black_args)
-    lint_ruff(ctx, ruff_args)
+    lint_black(ctx, args)
+    lint_ruff(ctx, args)
 
 
 @task(optional=["args"], help={"args": "check tools (pyright) additional arguments"})
@@ -96,14 +91,13 @@ def fix_ruff(ctx: Context, args: str = "."):
 
 
 @task(
-    optional=["black_args", "ruff_args"],
+    optional=["args"],
     help={
-        "black_args": "linting (fix mode) black arguments",
-        "ruff_args": "linting (fix mode) ruff arguments",
+        "args": "linting tools (black, ruff) additional arguments, typically a path",
     },
 )
-def fixall(ctx: Context, black_args: str = ".", ruff_args: str = "."):
+def fixall(ctx: Context, args: str = "."):
     """Fix everything automatically"""
-    fix_black(ctx, black_args)
-    fix_ruff(ctx, ruff_args)
-    lintall(ctx, black_args=black_args, ruff_args=ruff_args)
+    fix_black(ctx, args)
+    fix_ruff(ctx, args)
+    lintall(ctx, args)
